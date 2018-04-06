375327084a6a183e5e99bffd285a8850c2b15665
 Aleksey Panchenko <alekseypanchenko@MacBook-Pro-Aleksey.local>
2018-04-06 12:56:38 +0300

    delete search module by default

--- packages/core/src/modules/index.js
+++ packages/core/src/modules/index.js
@@ -10,13 +10,11 @@ import SecurityModule from 'makeen-security-module';
 import HealthModule from 'makeen-health';
 import PlayModule from './play';
 import AdminModule from './admin';
-import SearchModule from 'makeen-search';
 import AuditTrailModule from 'makeen-audit-trail';
 
 export default async config => [
   new OctobusModule(await config.get('modules.octobus')),
   new AuditTrailModule(await config.get('modules.auditTrail')),
-  new SearchModule(await config.get('modules.search')),
   new LoggerModule(await config.get('modules.logger')),
   new MongoDBModule(await config.get('modules.mongodb')),
   new UserModule(await config.get('modules.user')),
